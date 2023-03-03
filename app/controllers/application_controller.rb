3b1dc9d426cf44d88e311887b0314de7330e58de
leah wanjiku <leagm22@gmail.com>
2023-03-03 15:58:50 +0300

    git add data on controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,52 +15,24 @@ class ApplicationController < Sinatra::Base
     books.to_json
   end
   post '/favourites' do
-    book= Favourite.create (params)
+    request.body.rewind
+    request_payload = JSON.parse(request.body.read)
+    book = Favourite.create(book_id: request_payload['book_id'])
     book.to_json
   end
-    # book_id = params[:book_id]
-  
-    # if book_id.blank?
-    #   # Return an error message if book_id is empty
-    #   { error: "book_id cannot be empty" }.to_json
-    # else
-    #   # Create the new favorite
-    #   favourite = Favourite.find_or_create_by(book_id: book_id)
-  
-    #   if favourite.save
-    #     { message: "Book added to favorites", favourite: favourite }.to_json
-    #   else
-    #     { message: "Error adding book to favorites" }.to_json
-    #   end
-    # end
+  get '/favourites' do 
+    Favourite.all.to_json(include: :book)
+  end
+  delete '/favourites/:id' do
+    favourite = Favourite.find(params[:id])
+    favourite.destroy
   end
   
-  # post '/favourites' do
-  #   begin
-  #     puts "Incoming params: #{params.inspect}"
-  #     puts params.inspect
-  #     book_id = params[:book_id]
-  #     puts "book_id: #{book_id.inspect}"
-  #     favourite = Favourite.find_or_create_by(book_id: book_id)
-  #     if favourite.save
-  #       { message: "Book added to favorites", favourite: favourite }.to_json
-  #     else
-  #       { message: "Error adding book to favorites" }.to_json
-  #     end
-  #   rescue => e
-  #     { error: e.message }.to_json
-  #   end
-  # end
+end
+    
+   
+ 
   
-  
-#   get '/favourites' do
-#     favourites = Favourite.includes(:book).all
-#     content_type :json # set the content type to JSON
-#     favourites.to_json(include: { book: { only: [:id, :title, :description, :image_url] }})
-#   end
-  
-  
-# end
 
   
   
