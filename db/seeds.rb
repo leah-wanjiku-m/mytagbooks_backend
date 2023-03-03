e258f9067d32c009ec6e3581d6937af7a97edbd3
leah wanjiku <leagm22@gmail.com>
2023-03-03 11:14:12 +0300

    git add

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+puts "🌱 Seeding..."
+
+# Create books
+Book.create(title: 'To Kill a Mockingbird', description: 'A Pulitzer Prize-winning novel by Harper Lee.', image_url: 'https://pixabay.com/images/id-3088775/')
+Book.create(title: '1984', description: 'A dystopian novel by George Orwell.', image_url: 'https://pixabay.com/images/id-3088775/')
+Book.create(title: 'The Great Gatsby', description: 'A novel by F. Scott Fitzgerald.', image_url: 'https://pixabay.com/images/id-3088775/')
+Book.create(title: 'Pride and Prejudice', description: 'A novel by Jane Austen.', image_url: 'https://pixabay.com/images/id-3088775/')
+Book.create(title: 'One Hundred Years of Solitude', description: 'A novel by Gabriel Garcia Marquez.', image_url: 'https://pixabay.com/images/id-3088775/')
+Book.create(title: 'The Catcher in the Rye', description: 'A novel by J.D. Salinger.', image_url: 'https://pixabay.com/images/id-3088775/')
+Book.create(title: 'The Hobbit', description: 'A novel by J.R.R. Tolkien.', image_url: 'https://pixabay.com/images/id-3088775/')
+Book.create(title: 'Animal Farm', description: 'A novella by George Orwell.', image_url: 'https://pixabay.com/images/id-3088775/')
+Book.create(title: 'The Lord of the Rings', description: 'A trilogy of novels by J.R.R. Tolkien.', image_url: 'https://pixabay.com/images/id-3088775/')
+Book.create(title: 'The Da Vinci Code', description: 'A mystery novel by Dan Brown.', image_url: 'https://pixabay.com/images/id-3088775/')
+Book.create(title: 'Gone with the Wind', description: 'A novel by Margaret Mitchell.', image_url: 'https://pixabay.com/images/id-3088775/')
+Book.create(title: 'The Picture of Dorian Gray', description: 'A novel by Oscar Wilde.', image_url: 'https://pixabay.com/images/id-3088775/')
+Book.create(title: 'The Hitchhiker\'s Guide to the Galaxy', description: 'A series of comedic science fiction novels by Douglas Adams.', image_url: 'https://pixabay.com/images/id-3088775/')
+Book.create(title: 'Brave New World', description: 'A dystopian novel by Aldous Huxley.', image_url: 'https://pixabay.com/images/id-3088775/')
+Book.create(title: 'The Shining', description: 'A horror novel by Stephen King.', image_url: 'https://pixabay.com/images/id-3088775/')
+Book.create(title: 'Frankenstein', description: 'A novel by Mary Shelley.', image_url: 'https://pixabay.com/images/id-3088775/')
+Book.create(title: 'The Road', description: 'A post-apocalyptic novel by Cormac McCarthy.', image_url: 'https://pixabay.com/images/id-3088775/')
+puts "✅ Done seeding!"
